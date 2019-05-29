84ba7b708d4fac8df2114cff4741cc2206676dbe
chan Heo <celined06@gmail.com>
2019-05-29 12:10:44 +0900

    basic

---
+++ day1/go-basic/2.param/param.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+	testparam("2")
+}
+
+func testparam(a string) {
+	fmt.Println(a)
+}
---
+++ day1/go-basic/5.return_3/return.go
@@ -0,0 +1,16 @@
+package main 
+ 
+
+import "fmt" 
+ 
+
+ func main() { 
+ 	var1, _ := testReturn() 
+ 	fmt.Println(var1) 
+ 	//fmt.Println(var2) 
+ } 
+ 
+
+ func testReturn() (string, string) { 
+ 	return "3", "2" 
+ } 
